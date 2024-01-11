cd6bf28a7e4ba781489fcc728e1f4d97e75e28f1
Jonathan Ly <windevly@gmail.com>
2024-01-11 16:16:09 -0600

    fix: actually use the placeholder for input

--- app/components/text-input.tsx
+++ app/components/text-input.tsx
@@ -4,18 +4,19 @@ type TextInputProps = {
   name?: string;
   placeholder?: string;
   labelCompoennt?: React.ReactNode;
-};
+} & React.ComponentProps<'input'>;
 
 export default function TextInput(props: TextInputProps) {
-  const { type, labelCompoennt, placeholder, label, name } = props;
+  const { type, labelCompoennt, placeholder, label, name, className } = props;
 
   return (
-    <label>
+    <label className={className}>
       {labelCompoennt || label}
       <input
         type={type}
-        className='border border-gray-300 rounded-md py-1 px-2 w-full text-black active:outline-none focus:outline-none focus:ring-2 focus:ring-blue-500'
+        className='border border-gray-300 rounded-md py-1 px-2 w-full text-black active:outline-none focus:outline-none focus:ring-2 focus:ring-blue-500 bg-neutral-50'
         name={name}
+        placeholder={placeholder}
       />
     </label>
   );
